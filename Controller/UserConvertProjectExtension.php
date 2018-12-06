071a3e065b0407f0d9e5e430e692521e82448819
Craig Crosby <creecros@gmail.com>
2018-12-06 09:41:17 -0500

    Create UserConvertProjectExtension.php

---
+++ Controller/UserConvertProjectExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Kanboard\Plugin\ConvertProject\Controller;
+
+use Kanboard\Controller\ProjectEditController;
+use Kanboard\Validator\ProjectValidator;
+
+/**
+ * Project Edit Controller
+ *
+ * @package  Kanboard\Controller
+ * @author   Frederic Guillot
+ */
+class UserConvertProjectExtension extends ProjectEditController
+{
+
+public function cp_update()
+    {
+        $project = $this->getProject();
+        $values = $this->request->getValues();
+        $values = $this->cp_prepareValues($project, $values);
+        list($valid, $errors) = $this->projectValidator->validateModification($values);
+        if ($valid) {
+            if ($this->projectModel->update($values)) {
+                $this->flash->success(t('Project updated successfully.'));
+                return $this->response->redirect($this->helper->url->to('ProjectEditController', 'show', array('project_id' => $project['id'])), true);
+            } else {
+                $this->flash->failure(t('Unable to update this project.'));
+            }
+        }
+        return $this->show($values, $errors);
+    }
+    /**
+     * Prepare form values
+     *
+     * @access private
+     * @param  array  $project
+     * @param  array  $values
+     * @return array
+     */
+    private function cp_prepareValues(array $project, array $values)
+    {
+        $values['id'] = $project['id'];
+        if (isset($values['is_private'])) {
+                unset($values['is_private']);
+            }
+        } elseif ($project['is_private'] == 1 && ! isset($values['is_private'])) {
+                $values += array('is_private' => 0);
+            }
+        }
+        return $values;
+    }
+}
