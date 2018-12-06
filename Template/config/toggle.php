c6ba154b8a2253fb3f2dddd7a795e3541ca4c053
Craig Crosby <creecros@gmail.com>
2018-12-06 09:23:02 -0500

    Update toggle.php

--- Template/config/toggle.php
+++ Template/config/toggle.php
@@ -1,4 +1,4 @@
 <div class="panel">
-    <?= $this->form->radio('allow_project_convert_for_user', 'Hide Total Swimlane Task Counts in column headers' , 1, isset($values['allow_project_convert_for_user'])&& $values['allow_project_convert_for_user']==1) ?>
-    <?= $this->form->radio('allow_project_convert_for_user', 'Show Total Swimlane Task Counts in column headers' , 2, isset($values['allow_project_convert_for_user'])&& $values['allow_project_convert_for_user']==2) ?>
+    <?= $this->form->radio('allow_project_convert_for_user', 'Allow Users to convert their Private Project to Public' , 1, isset($values['allow_project_convert_for_user'])&& $values['allow_project_convert_for_user']==1) ?>
+    <?= $this->form->radio('allow_project_convert_for_user', 'Disallow Users to convert their Private Project to Public' , 2, isset($values['allow_project_convert_for_user'])&& $values['allow_project_convert_for_user']==2) ?>
 </div>
